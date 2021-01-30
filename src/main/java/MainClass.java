ba141cfd675f54e2718972df21ea6860cec03def
IvlevEV <iLynch47@gmail.com>
2021-01-30 16:52:49 +0600

    Да не красиво, исправлюсь

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,5 +1,5 @@
 public class MainClass {
     public static void main(String[] args){
-        ChatController chatController = new ChatController(); //вызов
+        ChatController chatController = new ChatController();
     }
 }
